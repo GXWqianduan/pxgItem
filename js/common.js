c6de636c28b400ccb3ed4eeba617f122e2e97222
GXWqianduan <115808777@qq.com>
2019-04-05 21:07:37 +0800

    添加tap事件解决mui框架中屏蔽单击事件

--- js/common.js
+++ js/common.js
@@ -1,4 +1,10 @@
 $(function () {
+   // click事件移动端有延迟,mui框架中屏蔽了单击事件,默认行为
+   // 解决方法是给a绑定tap事件,
+   mui('body').on('tap','a',function(e){
+      e.preventDefault();
+      window.top.location.href=this.href;
+   })
   // zepto导航拦截器
  $.ajaxSettings.beforeSend = function (xhr, obj) {
     const baseURL = 'http://157.122.54.189:9094/api/public/v1/';
